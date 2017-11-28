6a3f155b193d2a57951ed44f0a8ee76bb9d156b1
S00173244 <S00173244@mail.itsligo.ie>
2017-11-28 14:26:56 +0000

    Changed 1.0

--- Cart/Cart/MainWindow.xaml.cs
+++ Cart/Cart/MainWindow.xaml.cs
@@ -20,9 +20,65 @@ namespace Cart
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Product> productList = new List<Product>();
+        List<Product> cartList = new List<Product>();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Product product1 = new Product("1234", "book", 12.53);
+            productList.Add(product1);
+            Product product2 = new Product("1235", "bike", 780.00);
+            productList.Add(product2);
+            Product product3 = new Product("1236", "boat", 900.00);
+            productList.Add(product3);
+
+            lbox_products.ItemsSource = productList;
+
+
+        }
+
+        private void btn_addTax_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Product item in productList)
+            {
+                item.AddTax(0.03);
+
+            }
+            lbox_products.ItemsSource = "";
+            lbox_products.ItemsSource = productList;
+        }
+
+        private void btn_addToCart_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbox_products.SelectedItem != null)
+            {
+
+                if (!cartList.Contains(lbox_products.SelectedItem as Product)) cartList.Add(lbox_products.SelectedItem as Product);
+
+                lbox_cart.ItemsSource = "";
+                lbox_cart.ItemsSource = cartList;
+            }
+
+
+        }
+
+        private void btn_rmvFromCart_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (lbox_cart.SelectedItem != null)
+            {
+
+                cartList.Remove(lbox_cart.SelectedItem as Product);
+
+                lbox_cart.ItemsSource = "";
+                lbox_cart.ItemsSource = cartList;
+            }
+        }
+    
     }
 }
---
+++ Cart/Cart/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart
+{
+    class Product
+    {
+        string id;
+        string name;
+        double price;
+        double tax;
+
+        public Product(string i,string n, double p)
+        {
+            id = i;
+            name = n;
+            price = p;
+        }
+
+        public override string ToString()
+        {
+            if(tax!=0) return id + "\t" + name + "\t" + "€"+price + "\t" + tax;
+            else return id + "\t" + name + "\t" + "€" + price;
+        }
+
+        public void AddTax(double taxRate)
+        {
+
+
+
+            tax = price * taxRate;
+            
+        }
+    }
+}
